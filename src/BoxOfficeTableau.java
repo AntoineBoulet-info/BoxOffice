8d1d42daa5d9c2b1aaf4d1949b9ba1ad090f80db
AntoineBoulet-info <antoineboulet6@gmail.com>
2020-12-02 15:46:02 +0100

    addFilm

--- src/BoxOfficeTableau.java
+++ src/BoxOfficeTableau.java
@@ -1,48 +1,53 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class BoxOfficeTableau extends BoxOffice {
 
     public final static int TAILLE_INITIALE= 100;
-    private Film[] elements;//contient les infos d'un Film
+    public static int TAILLE_TMP = TAILLE_INITIALE;
+    private static Film[] elements = new Film[TAILLE_INITIALE];//contient les infos d'un Film
     private static int nbFilms;
     private static int nbLignes;
 
 
-
+    //méthode
     public void addFilm(String titre, String realisateur, int annee, int nbEntrees) {
-        for (int i=0;i<elements.length;i++){
-            if (elements.length > TAILLE_INITIALE) {
+            if (nbFilms == TAILLE_TMP) {
+                //augmente la taille si le nombre de films dépasse la taille du tableau
+                TAILLE_TMP = TAILLE_TMP * 2;
+                Film[] tab = elements;
+                elements = new Film[TAILLE_TMP];
+                for(int i=0;i< tab.length ;i++) {
+                    Film film = new Film(titre, realisateur, annee, nbEntrees);
+                    elements[i++] = film;
+                }
 
-            }
         }
+        elements[nbFilms++] = new Film(titre, realisateur, annee, nbEntrees);
 
     }
 
+    //constructeur
     public BoxOfficeTableau(String listing) {
         super(listing);
-        this.elements = new Film[TAILLE_INITIALE];
     }
 
 
-
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         if (args.length<1) System.err.println("nom de fichier manquant");
         else new BoxOfficeTest(args[0]);
-
         try {
             Scanner in = new Scanner(new File(args[0]));
             String line;
-            String titre_film;
             while(in.hasNextLine()) {
                 line = in.nextLine();
                 nbLignes++;
-
             }
+            in.close();
 
         }
-
 
         catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -51,8 +56,9 @@ public class BoxOfficeTableau extends BoxOffice {
         System.out.println("Fichier : "+ args[0]);
         System.out.println("Nombre de lignes : " + nbLignes);
         System.out.println("Nombre de films : " + nbFilms );
-        System.out.println("--------");
+        System.out.println("------------");
         System.out.println("Films comptabilisant le plus grand nombre d'entrees :");
+
 
     }
 }
